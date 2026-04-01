d651d85557745ce91d8e676eafd4e8783fed4b81
VishnuVardhan21 <nvishnuvardhanreddy63@gmail.com>
2026-04-01 21:49:56 +0530

    deployment fixes

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -7,8 +7,8 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: 'https://stockflow-otcfpsenj-vishnuvardhanreddy-ns-projects.vercel.app',
-        credentials: true,
+        origin: '*',
+        credentials: false,
     });
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true, transform: true }));
     app.setGlobalPrefix('api');
