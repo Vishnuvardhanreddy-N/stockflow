d2ce28d155b9af788b398ebb98d2b2774a1febc4
VishnuVardhan21 <nvishnuvardhanreddy63@gmail.com>
2026-04-01 21:41:53 +0530

    deployment fixes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  });
+  origin: 'https://stockflow-otcfpsenj-vishnuvardhanreddy-ns-projects.vercel.app',
+  credentials: true,
+});
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('api');
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://stockflow-production-b332.up.railway.app/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('sf_token');
